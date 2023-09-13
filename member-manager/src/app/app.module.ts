cd01fea698211f234d13bae17ee1654b65bfbc9a
Aidan Killeen <aidan.killeen@gmail.com>
2023-09-13 16:12:24 +0100

    added member list page

--- member-manager/src/app/app.module.ts
+++ member-manager/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule }  from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
@@ -27,7 +28,8 @@ import { MemberDetailPageComponent } from './pages/member-detail-page/member-det
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule, 
+    HttpClientModule,
+    FormsModule,  
     AppRoutingModule, 
     ButtonModule, 
     MenubarModule, 
--- member-manager/src/app/pages/member-list-page/member-list-page.component.ts
+++ member-manager/src/app/pages/member-list-page/member-list-page.component.ts
@@ -7,22 +7,64 @@ import { MemberService } from 'src/app/services/member.service';
   selector: 'member-list-page',
   template: `
 
-    <p-table [value]="(members$ | async)!">
+    <p-table 
+      dataKey="id" 
+      editMode="row"
+      [value]="(members$ | async)!">
       <ng-template pTemplate="header">
         <tr>
           <th>Id</th>
           <th>Name</th>
           <th>Email</th>
           <th>Active</th>
+          <th>Controls</th>
         </tr>
       </ng-template>
 
-      <ng-template pTemplate="body" let-member>
-        <tr>
+      <ng-template pTemplate="body" let-member let-editing="editing" let-ri="rowIndex">
+        <tr [pEditableRow]="member">
           <td>{{ member.id }}</td>
-          <td>{{ member.name }}</td>
+          <td>
+            <p-cellEditor>
+              <ng-template pTemplate="input">
+                <input pInputText type="text" [(ngModel)]="member.name">
+              </ng-template>
+              <ng-template pTemplate="output">
+                {{ member.name }}
+              </ng-template>
+
+            </p-cellEditor>
+          </td>
           <td>{{ member.email }}</td>
           <td>{{ member.active ? "Active" : "Inactive" }}</td>
+          <td>
+            <button 
+              *ngIf="!editing" 
+              pButton pRipple 
+              type="button" 
+              pInitEditableRow 
+              (click)="onRowEditInit(member)"
+              icon="pi pi-pencil" 
+              class="p-button-rounded p-button-text">
+            </button>
+            <button 
+              *ngIf="editing" 
+              pButton pRipple 
+              type="button" 
+              pSaveEditableRow 
+              icon="pi pi-check" 
+              class="p-button-rounded p-button-text p-button-success mr-2">
+            </button>
+            <button 
+              *ngIf="editing" 
+              pButton pRipple 
+              type="button" 
+              pCancelEditableRow 
+              icon="pi pi-times" 
+              class="p-button-rounded p-button-text p-button-danger">
+            </button>
+ 
+          </td>
         </tr>
       </ng-template>
     </p-table>
@@ -33,11 +75,16 @@ export class MemberListPageComponent implements OnInit {
 
   members$!: Observable<Member[]>;
 
+  clonedMembers: { [s: string]: Member } = {};
+
   constructor(public memberService: MemberService) {
   }
   ngOnInit(): void {
     this.members$ = this.memberService.getAll();
   }
 
+  onRowEditInit(member: Member) {
+    this.clonedMembers[`${member.id}`] = { ...member };
+  }
 
 }
